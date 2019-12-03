078ee110f63c0d27f6a71aa556a6dd3e574f5256
m4cu4rr0 <lefc_11@me.com>
2019-12-03 14:11:47 -0600

    1 file modified

--- src/app/cuestionario/quest3/quest3.component.ts
+++ src/app/cuestionario/quest3/quest3.component.ts
@@ -100,8 +100,178 @@ export class Quest3Component implements OnInit {
   }
 
   siguiente() {
-    window.scroll(0, 0);
-    this.seccion++;
+    if(this.seccion == 1){
+      if(this.quest3.preg1 === null ||
+        this.quest3.preg2 === null ||
+        this.quest3.preg3 === null ||
+        this.quest3.preg4 === null ||
+        this.quest3.preg5 === null ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 2) {
+      if(this.quest3.preg6 === null ||
+        this.quest3.preg7 === null ||
+        this.quest3.preg8 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 3) {
+      if(this.quest3.preg9 === null ||
+        this.quest3.preg10 === null ||
+        this.quest3.preg11 === null ||
+        this.quest3.preg12 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 4) {
+      if(this.quest3.preg13 === null ||
+        this.quest3.preg14 === null ||
+        this.quest3.preg15 === null ||
+        this.quest3.preg16 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 5) {
+      if(this.quest3.preg17 === null ||
+        this.quest3.preg18 === null ||
+        this.quest3.preg19 === null ||
+        this.quest3.preg20 === null ||
+        this.quest3.preg21 === null ||
+        this.quest3.preg22 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 6) {
+      if(this.quest3.preg23 === null ||
+        this.quest3.preg24 === null ||
+        this.quest3.preg25 === null ||
+        this.quest3.preg26 === null ||
+        this.quest3.preg27 === null ||
+        this.quest3.preg28 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 7) {
+      if(this.quest3.preg29 === null ||
+        this.quest3.preg30 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 8) {
+      if(this.quest3.preg31 === null ||
+        this.quest3.preg32 === null ||
+        this.quest3.preg33 === null ||
+        this.quest3.preg34 === null ||
+        this.quest3.preg35 === null ||
+        this.quest3.preg36 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 9) {
+      if(this.quest3.preg37 === null ||
+        this.quest3.preg38 === null ||
+        this.quest3.preg39 === null ||
+        this.quest3.preg40 === null ||
+        this.quest3.preg41 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 10) {
+      if(this.quest3.preg42 === null ||
+        this.quest3.preg43 === null ||
+        this.quest3.preg44 === null ||
+        this.quest3.preg45 === null ||
+        this.quest3.preg46 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 11) {
+      if(this.quest3.preg47 === null ||
+        this.quest3.preg48 === null ||
+        this.quest3.preg49 === null ||
+        this.quest3.preg50 === null ||
+        this.quest3.preg51 === null ||
+        this.quest3.preg52 === null ||
+        this.quest3.preg53 === null ||
+        this.quest3.preg54 === null ||
+        this.quest3.preg55 === null ||
+        this.quest3.preg56 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 12) {
+      if(this.quest3.preg57 === null ||
+        this.quest3.preg58 === null ||
+        this.quest3.preg59 === null ||
+        this.quest3.preg60 === null ||
+        this.quest3.preg61 === null ||
+        this.quest3.preg62 === null ||
+        this.quest3.preg63 === null ||
+        this.quest3.preg64 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 13) {
+      if(this.quest3.preg65 === null ||
+        this.quest3.preg66 === null ||
+        this.quest3.preg67 === null ||
+        this.quest3.preg68 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+        }
+    } else if (this.seccion == 14) {
+      if(this.quest3.preg69 === null ||
+        this.quest3.preg70 === null ||
+        this.quest3.preg71 === null ||
+        this.quest3.preg72 === null  ){
+          this.error = 1 ;
+        } else {
+          this.error = 0;
+          window.scroll(0, 0);
+          this.seccion++;
+          this. salir()
+        }
+    }
   }
 
   salir() {
@@ -112,18 +282,20 @@ export class Quest3Component implements OnInit {
   clienteChange($event){
     console.log(this.serCliente);
     if(this.serCliente == 0){
-      this.quest3.preg41 = '0';
-      this.quest3.preg42 = '0';
-      this.quest3.preg43 = '0';
+      this.quest3.preg65 = '0';
+      this.quest3.preg66 = '0';
+      this.quest3.preg67 = '0';
+      this.quest3.preg68 = '0';
     }
   }
 
   jefeChange($event){
     console.log(this.esJefe);
     if(this.esJefe == 0){
-      this.quest3.preg44 = '0';
-      this.quest3.preg45 = '0';
-      this.quest3.preg46 = '0';
+      this.quest3.preg69 = '0';
+      this.quest3.preg70 = '0';
+      this.quest3.preg71 = '0';
+      this.quest3.preg72 = '0';
     }
   }
 
